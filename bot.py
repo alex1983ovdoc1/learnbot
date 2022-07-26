b58a921032071e9d4a49c05edbd29773c8554bc6
alex3 <bondar1983ovdoc1@gmail.com>
2022-07-26 15:06:00 +0300

    Bot add planet

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 import settings123
 
+
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 					level=logging.INFO,
 					filename='bot.log')
@@ -16,6 +17,12 @@ def greet_user(update: Update, context: CallbackContext):
 	print('--------------')
 	logging.info(text)
 	# update.message.reply_text(text)
+	update.message.reply_text(
+        "Please write:\
+	\n -/start\
+	\n -/any_text \
+	\n -/planet")
+	
 	
 def talk_to_me(update: Update, context: CallbackContext):
 	user_text = update.message.text
@@ -25,31 +32,59 @@ def talk_to_me(update: Update, context: CallbackContext):
 	print('--------------')
 	print(text_print)
 	print('--------------')
-	# logging.info('--------------')
-	# logging.info(text_print)
-	logging.info('--------------')
+
 	# print(update.message)
-	logging.info(f'User: {update.message.chat.first_name}, \
-				Chat id: {update.message.chat.id}, \
-				Message: {update.message.text}')
-	logging.info('--------------')
+	logging.info(f'User:{update.message.chat.first_name},\
+	Chat id:{update.message.chat.id},\
+	Message:{update.message.text}')
 	update.message.reply_text(text_print)
+
+def planet_list(update: Update, context: CallbackContext):
+	update.message.reply_text(
+        "Please select object:\
+	\n -/Sun\n -/Moon\n -/Mercury\n -/Venus\n -/Mars\n -/Jupiter\n -/Saturn\n -/Uranus\n -/Neptune\n -/Pluto")
+	
+def planet_user(update: Update, context: CallbackContext):
+	import planet_object
+	# print(planet_object.planet1)
+	Message_pl = update.message.text
+	# print(Message_pl)
+	select_planet=Message_pl.replace('/','')
+	# print(select_planet)
+	select_date = planet_object.time_in
+	select_coordinat = planet_object.planet1[select_planet]
+	print('--------------')
+	print(f'Planet	: {select_planet}')
+	print(f'Date	: {select_date}')
+	print(f'Location: {select_coordinat}')
+	print('--------------')
+	
+	logging.info(f'User:{update.message.chat.first_name},\
+	Chat id:{update.message.chat.id},\
+	Planet :{select_planet},\
+	Date   :{select_date},\
+	Location: {select_coordinat} ')
 
 # Функция, которая соединяется с платформой Telegram, тело нашего бота
 def main():
-	# mybot = Updater((API_KEY, request_kwargs = PROXY)
-	mybot = Updater(settings123.API_KEY)
+		# mybot = Updater((API_KEY, request_kwargs = PROXY)
+		mybot = Updater(settings123.API_KEY)
+		dp = mybot.dispatcher
 
-	logging.info('Bot starting')
-	print('--------------')
-	print('Bot starting')
-	print('--------------')
-
-	dp = mybot.dispatcher
-	dp.add_handler(CommandHandler('start', greet_user))
-	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
-	mybot.start_polling()
-	mybot.idle()
+		logging.info('Bot starting')
+		print('--------------')
+		print('Bot starting')
+		print('--------------')
+		
+		dp.add_handler(CommandHandler('start', greet_user))
+		dp.add_handler(CommandHandler('planet', planet_list))
+		dp.add_handler(CommandHandler(['Sun','Moon','Mercury','Venus','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto'], planet_user))
+		dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+		
+		mybot.start_polling()
+		mybot.idle()
+		
+	
 # Вызываем функцию - эта строчка запускает бот
 main()
+
